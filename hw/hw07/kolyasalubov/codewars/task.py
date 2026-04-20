bc43df45b206d957e20e8d6e99008fe791aa30a8
Liubov <lkoliasa@gmail.com>
2026-04-20 19:10:22 +0300

    added hw codewars

---
+++ hw/hw07/kolyasalubov/codewars/task.py
@@ -0,0 +1,13 @@
+print("codewars")
+
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {name}!".format(name=name)
+
+##############################################
+
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {name}!".format(name=name)
