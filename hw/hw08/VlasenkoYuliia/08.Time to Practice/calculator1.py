d25847be30133d7f2a0cff44a23863c8040d09f7
Vlasenko Yuliia <vlasenko.yuliia@gmail.com>
2026-05-02 21:02:14 +0200

    added: hw 08.2, 08.3

---
+++ hw/hw08/VlasenkoYuliia/08.Time to Practice/calculator1.py
@@ -0,0 +1,13 @@
+def addition(a: float, b: float) -> float:
+    return a + b
+
+def substraction(a: float, b: float) -> float:
+    return a - b
+
+def multiplication(a: float, b: float) -> float:
+    return a * b
+
+def division(a: float, b: float) -> float:
+    return round(a/b, 2)
+
+
